0173b7bde00fd56e700c4d10bcb2058f1fe5f07a
josMuiruri <josemuiruri51@gmail.com>
2025-06-13 15:34:40 +0300

    adding a serializer

--- my_site/product/views.py
+++ my_site/product/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from . models import Product
+from . serializers import ProductSerializer
 
 # Create your views here.
+def product_list(request):
+    product = product.objects.all()
+    serializer = ProductSerializer(product, many=True)
