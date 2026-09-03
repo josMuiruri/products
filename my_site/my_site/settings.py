533bf8e58ebfc0cbbdc635ecf1dc36caa2377ec1
josMuiruri <josemuiruri51@gmail.com>
2026-09-03 23:39:04 +0300

    create a compose file

--- my_site/my_site/settings.py
+++ my_site/my_site/settings.py
@@ -29,8 +29,10 @@ SECRET_KEY = env.str('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', default=False)
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
-
+ALLOWED_HOSTS = env.list(
+    'ALLOWED_HOSTS',
+    default=['localhost', '127.0.0.1']
+)
 
 # Application definition
 
